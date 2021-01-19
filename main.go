cb70c67a0d225b5a960ff1d79fc9bdb72b6a1353
Zanuardi Novanda <zanuardinovanda@gmail.com>
2021-01-19 13:48:23 +0700

    fix main.go

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/zanuardinovanda/go-testing/controllers"
+	"github.com/zanuardinovanda/go-testing/models"
 )
 
 func main() {
 
 	route := gin.Default()
+
+	//Model
+	db := models.SetupModels()
+	route.Use(func(c *gin.Context) {
+		c.Set("db", db)
+		c.Next()
+	})
 
 	route.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "Welcome to my API"})
